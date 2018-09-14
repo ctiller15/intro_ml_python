441ab1529f828b0eaed4548403c63baae52615ee
ctiller15 <christiller2015@gmail.com>
2018-09-14 14:54:13 -0400

    Finish ridge regression notes. Learn about alpha values

--- Chapter_2/LinearModels.py
+++ Chapter_2/LinearModels.py
@@ -1,5 +1,42 @@
 import matplotlib.pyplot as plt
+import sklearn
 import mglearn
+
+from sklearn.model_selection import train_test_split
 
 mglearn.plots.plot_linear_regression_wave()
 plt.show()
+
+# Linear regression/ ordinary least squares
+
+from sklearn.linear_model import LinearRegression
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lr = LinearRegression().fit(X_train, y_train)
+
+print("lr.coef_:{}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+# Training set score: 0.67
+# Test set score: 0.66
+# We've underfitted.
+
+# Boston housing dataset.
+
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+# Training set score: 0.95
+# Test set score: 0.61
+# The training set and test set are both way off.
+# We've overfitted.
+# We need a model that allows us to control complexity.
---
+++ Chapter_2/RidgeRegression.py
@@ -0,0 +1,47 @@
+import mglearn
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+
+
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
+
+# Training set score: 0.89
+# Test set score: 0.75
+
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge10.score(X_test, y_test)))
+# Training set score: 0.79
+# Test set score: 0.64
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge01.score(X_test, y_test)))
+# Training set score: 0.93
+# Test set score: 0.77
+
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lr.coef_))
+plt.ylim(-25, 25)
+plt.legend()
+plt.show()
+
+mglearn.plots.plot_ridge_n_samples()
+plt.show()
