daa984c614e68e71fc07f208950498e1ccb2d9a1
ctiller15 <christiller2015@gmail.com>
2018-09-15 00:27:35 -0400

    Begin working with lasso regression

---
+++ Chapter_2/LassoRegression.py
@@ -0,0 +1,55 @@
+import mglearn
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+
+lasso = Lasso().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+# Training set score: 0.29
+# Test set score: 0.21
+# Number of features used: 4
+
+# we increase the default setting of "max_iter"
+# otherwise the model would warn us that we should increase max_iter.
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+
+# Training set score: 0.90
+# Test set score: 0.77
+# Number of features used: 33
+
+# setting alpha too low...
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso00001.coef_ != 0)))
+
+# Training set score: 0.95
+# Test set score: 0.64
+# Number of features used: 94
+
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lasso001.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, 'v', label="Lasso alpha=0.0001")
+
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.show()
